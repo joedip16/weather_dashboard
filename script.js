6814f8dff656a037a8363137f7c382bba7e36783
joedip16 <joedippolito16@gmail.com>
2020-11-17 17:15:05 -0500

    created ajax call

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+    $("#search-button").on("click", function() {
+        var searchValue = $("#search-value").val();
+
+        $("#search-value").val("");
+
+        searchWeather(searchValue)
+    });
+
+
+    function searchWeather(searchValue){
+        $.ajax({
+            type: "GET",
+            url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=891d6bcf73691e3f5340876c34a589c2&units=imperial",
+            dataType: "json",
+        }).then(function(data){
+        console.log(data)
+
+        //create a history link for the search (look up .push()
+
+    }
+
+     )};
 
 
 
@@ -20,8 +43,4 @@ $(document).ready(function(){
 
 
 
-
-
-
-    
 })
